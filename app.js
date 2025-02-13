fa80f5bafc6d23819674a65a3d38b449c61a301d
Abhijeet Ugale <ugaleabhijeet03@gmail.com>
2025-02-13 18:45:16 +0530

    file uploaded
    tik tac toe game

---
+++ app.js
@@ -0,0 +1,80 @@
+const screenshow = document.querySelector('.game-status');
+let gameActive = true;
+let currentPlayer = "X";
+let gameState = ["", "", "", "", "", "", "", "", ""];
+
+const winningMessage = () => `Player ${currentPlayer} has won!`;
+const drawMessage = () => `its a draw!`;
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+
+screenshow.innerHTML = currentPlayerTurn();
+
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
+}
+
+function handlePlayerChange() {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    screenshow.innerHTML = currentPlayerTurn();
+}
+
+function handleResultValidation() {
+    let roundWon = false;
+
+    for (let i = 0; i < winningConditions.length; i++) {
+        const [a, b, c] = winningConditions[i];
+        if (gameState[a] !== "" && gameState[a] === gameState[b] && gameState[a] === gameState[c]) {
+            roundWon = true;
+            break;
+        }
+    }
+
+    if (roundWon) {
+        screenshow.innerHTML = winningMessage();
+        gameActive = false;
+        return;
+    }
+
+    if (!gameState.includes("")) {
+        screenshow.innerHTML = drawMessage();
+        gameActive = false;
+        alert(" Match is Drawn");
+        return;
+    }
+
+    handlePlayerChange();
+}
+
+function handleCellClick(event) {
+    const clickedCell = event.target;
+    const clickedCellIndex = Array.from(clickedCell.parentNode.children).indexOf(clickedCell);
+
+    if (gameState[clickedCellIndex] !== "" || !gameActive) return;
+
+    handleCellPlayed(clickedCell, clickedCellIndex);
+    handleResultValidation();
+}
+
+function handleRestartGame() {
+    gameActive = true;
+    currentPlayer = "X";
+    gameState = ["", "", "", "", "", "", "", "", ""];
+    screenshow.innerHTML = currentPlayerTurn();
+
+    document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
+}
+
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
+document.querySelector('.game-restart').addEventListener('click', handleRestartGame);
